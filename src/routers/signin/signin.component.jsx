9c1231219510f2bb77b959f7e7b7f5361632feb1
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-02-18 11:52:23 +0700

    Add redirect login

--- src/routers/signin/signin.component.jsx
+++ src/routers/signin/signin.component.jsx
@@ -1,14 +1,30 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+import {
+  auth,
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInWithGoogleRedirect
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-  const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup();
+  useEffect(() => {
+    const getResult = async () => {
+      const res = await getRedirectResult(auth);
+      createUserDocumentFromAuth(res.user);
+    }
+    getResult();
+  }, []);
+
+  async function logGoogleUser() {
+    const { user } = await signInWithGooglePopup();
     createUserDocumentFromAuth(user);
-  };
+  }
 
   return (
     <div>
-      <button onClick={logGoogleUser}>Click here to SignIn</button>
+      <button onClick={logGoogleUser}>Click here to SignIn Popup</button>
+      <button onClick={signInWithGoogleRedirect}>Click here to SignIn redirect</button>
     </div>
   );
 };
