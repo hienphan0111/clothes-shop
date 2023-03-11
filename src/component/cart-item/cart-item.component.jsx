29e776b049418d63b0ecd44377351ececc10f274
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-03-11 21:42:46 +0700

    implement add product to cart

---
+++ src/component/cart-item/cart-item.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CartItem = ({cartItem}) => {
+  const { }
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default CartItem;
--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,5 +1,18 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id)
+
+  if(existingCartItem) {
+    return cartItems.map((cartItem) => 
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  return [...cartItems, {...productToAdd, quantity: 1}]
+}
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
@@ -7,8 +20,13 @@ export const CartContext = createContext({
 
 export const CartProvider = ({children}) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const [cartItem, setCartItem] = useState('');
-  const val = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState('');
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd))
+  }
+
+  const val = { isCartOpen, setIsCartOpen, addItemToCart};
 
   return (
     <CartContext.Provider value={val}>{children}</CartContext.Provider>
